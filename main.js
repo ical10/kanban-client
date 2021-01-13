e489bd03f9bdf61589327f74d2b06ee4156be5a0
ical10 <mochhusnir@gmail.com>
2021-01-13 11:20:36 +0700

    styling login and register page, adding main.js for vue

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        tasks: [{ id: 1, category: "Backlog" }, { id: 2, category: "Todo" }, { id: 3, category: "Doing" }, { id: 4, category: "Done" }]
+    },
+    methods: {
+        removeTask(taskId) {
+            this.task
+        }
+    }
+})
